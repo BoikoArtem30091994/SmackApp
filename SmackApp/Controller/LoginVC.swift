7d6fe71380c473002dd1962007509c54cb9b826a
Artem <boykoartem94@gmail.com>
2017-10-08 13:50:41 +0300

    ChannelVC and LoginVC done

---
+++ SmackApp/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  SmackApp
+//
+//  Created by Boiko Artem on 06.10.17.
+//  Copyright © 2017 Boiko Artem. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //Actions
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ SmackApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmackApp
+//
+//  Created by Boiko Artem on 06.10.17.
+//  Copyright © 2017 Boiko Artem. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
+
