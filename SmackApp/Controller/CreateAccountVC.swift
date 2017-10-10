f8f833717a857f02b6e10c14a07dd082bcc9f316
Artem <boykoartem94@gmail.com>
2017-10-10 21:15:10 +0300

    Creating user logged function with SwiftyJSON

--- SmackApp/Controller/CreateAccountVC.swift
+++ SmackApp/Controller/CreateAccountVC.swift
@@ -9,13 +9,43 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // MARK: Actions
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else {return}
+        guard let pass = passTxt.text, passTxt.text != "" else {return}
+        
+        AuthServices.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                AuthServices.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("logged user", AuthServices.instance.authToken)
+                    }
+                })
+            }
+        }
+    }
     
+    
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+
 }
--- SmackApp/Utilities/Constants.swift
+++ SmackApp/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 //Url Constants
 let BASE_URL =  "https://smackyapp.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 
 //Segues
@@ -24,3 +25,7 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Constants headers
+let HEADER = [
+    "Content-Type" : "application/json;charset=utf-8"]
