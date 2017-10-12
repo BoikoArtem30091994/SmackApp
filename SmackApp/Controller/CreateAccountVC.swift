e908c78334735ca2d05cd6c1d8352a68dad98f7f
Artem <boykoartem94@gmail.com>
2017-10-12 17:51:47 +0300

    All api func created:login user, register user and add user

--- SmackApp/Controller/CreateAccountVC.swift
+++ SmackApp/Controller/CreateAccountVC.swift
@@ -17,6 +17,10 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImage: UIImageView!
     
+    // MARK: Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -26,6 +30,7 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     @IBAction func createAccntPressed(_ sender: Any) {
+        guard let name = userNameTxt.text, userNameTxt.text != "" else {return}
         guard let email = emailTxt.text, emailTxt.text != "" else {return}
         guard let pass = passTxt.text, passTxt.text != "" else {return}
         
@@ -33,7 +38,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthServices.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("logged user", AuthServices.instance.authToken)
+                        AuthServices.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
---
+++ SmackApp/Services/UserDataService.swift
@@ -0,0 +1,33 @@
+//
+//  UserDataService.swift
+//  SmackApp
+//
+//  Created by Boiko Artem on 12.10.17.
+//  Copyright © 2017 Boiko Artem. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String){
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+    
+}
