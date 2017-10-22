dd7d14df24d83b1abb145afba97b7ed5b02ba279
Artem <boykoartem94@gmail.com>
2017-10-22 14:48:06 +0300

    Channels table view implement

--- SmackApp/Controller/ChannelVC.swift
+++ SmackApp/Controller/ChannelVC.swift
@@ -8,24 +8,29 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         setupUserInfo()
     }
-
+    
     //Action
     @IBAction func loginBtnPressed(_ sender: Any) {
         if AuthServices.instance.isLogged {
@@ -34,9 +39,9 @@ class ChannelVC: UIViewController {
             profile.modalPresentationStyle = .custom
             present(profile, animated: true , completion: nil)
         } else {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+            performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
     }
-}
     
     @objc func userDataDidChange(_ notif: Notification) {
         setupUserInfo()
@@ -52,6 +57,25 @@ class ChannelVC: UIViewController {
             userImg.image = UIImage(named: "menuProfileIcon")
             userImg.backgroundColor = UIColor.clear
         }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell{
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
     }
 }
 
--- SmackApp/View/ChannelCell.swift
+++ SmackApp/View/ChannelCell.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ChannelCell: UITableViewCell {
-
+    
+    //Outlet
+    @IBOutlet weak var channelName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +20,15 @@ class ChannelCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    func configureCell(channel: Channel){
+        let title = channel.channelTitle ?? ""
+        channelName.text = title
     }
 
 }
